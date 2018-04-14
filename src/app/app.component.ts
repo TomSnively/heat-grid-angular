78a3e28dd54f3fb539ca2c0e7032fd82675e3fef
Tom Snively <voxbop@gmail.com>
2018-04-14 12:40:11 -0400

    Setting up variables, and initializing most of them.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Initial state
+  gridSize:number = 11;
+  heatIncrease:number = 1;
+  intervalSpeed:number = 5;
+  maxHeat:number = 31;
+  timerRunning:boolean = false;
+  setTimeoutId:number = null;
+  totalHeat:number = 0;
+  cellArrays:Cell[][];
+
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+    let size = this.gridSize;
+
+    let grid = new Array(size + 1);
+    for (let i=0; i <= size + 1; i++) {
+        grid[i] = new Array(size + 1);
+    }
+
+
+  }
+
+
+}
+
+interface Cell {
+  temperature:Number;
+  lasttemp:Number;
+  selected:boolean;
 }
