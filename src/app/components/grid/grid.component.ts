79f5bb255d8a0c00eb8b61c2ea8a85ae74a45aba
Tom Snively <voxbop@gmail.com>
2018-04-16 15:19:28 -0400

    Successfully passed gridSize from app to grid, and pass rowNumber from grid to row. Added CSS

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
-  constructor() { }
+  @Input() gridSize: number;
+  rows: number[];
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    console.log('gridSize', this.gridSize);
+    this.rows = Array(this.gridSize).fill(0).map((x,i)=>i+1);
+    
   }
 
 }
--- src/app/components/row/row.component.ts
+++ src/app/components/row/row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-row',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./row.component.css']
 })
 export class RowComponent implements OnInit {
+
+  @Input() rowNumber: number;
 
   constructor() { }
 
